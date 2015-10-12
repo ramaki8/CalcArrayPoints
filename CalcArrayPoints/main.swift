3851fc3ed5ff3bec7b18d1dbf23360f2746d2e97
Rachel Kipps <rachelkipps@gmail.com>
2015-10-12 11:22:25 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Rachel Kipps on 10/12/15.
+//  Copyright © 2015 Rachel Kipps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
